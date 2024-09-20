552510f275808a5f01c1b74a011c9fa730525861
Rishabh Raj <rishabhraj.clg@gmail.com>
2024-09-20 22:38:18 +0530

    worked on error codes and made update and delete request

--- blog/main.py
+++ blog/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, status, Response, HTTPException
 from . import schemas, models
 from .database import engine, SessionLocal
 from sqlalchemy.orm import Session
@@ -15,13 +15,33 @@ def get_db():
         db.close()
     
 
-@app.post('/blog')
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Blog, db:Session=Depends(get_db)):
     new_blog = models.Blog(title=request.title, body=request.body)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
+
+@app.delete('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND)
+def destroy(id, db: Session = Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog.first():
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id: {id} not found")
+    blog.delete(synchronize_session=False)
+    db.commit()
+    return {'deleted'}
+
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
+    blog=db.query(models.Blog).filter(models.Blog.id == id)
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id: {id} not found")
+    print(request.title)
+    blog.update(dict(request))
+    db.commit()
+    return 'updated'
+
 
 @app.get('/blog')
 def all(db: Session = Depends(get_db)):
@@ -30,5 +50,9 @@ def all(db: Session = Depends(get_db)):
     
 @app.get('/blog/{id}')
 def show(id, db: Session = Depends(get_db)):
-    blogs=db.query(models.Blog).filter(models.Blog.id==id).first()
-    return blogs
+    blog=db.query(models.Blog).filter(models.Blog.id==id).first()
+    if not blog:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id: {id} is not available.")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f"Blog with id: {id} is not available."}
+    return blog
