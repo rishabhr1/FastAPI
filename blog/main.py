c7a4d1e55d0ed52bcee2041fee53be2bbb626885
Rishabh Raj <rishabhraj.clg@gmail.com>
2024-09-22 11:41:27 +0530

    implemented response model, create user with hash password and relationship

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,8 @@
 from fastapi import FastAPI, Depends, status, Response, HTTPException
+from typing import List
 from . import schemas, models
 from .database import engine, SessionLocal
+from .hashing import Hash
 from sqlalchemy.orm import Session
 
 app=FastAPI()
@@ -15,15 +17,15 @@ def get_db():
         db.close()
     
 
-@app.post('/blog', status_code=status.HTTP_201_CREATED)
+@app.post('/blog', status_code=status.HTTP_201_CREATED, tags=['blogs'])
 def create(request: schemas.Blog, db:Session=Depends(get_db)):
-    new_blog = models.Blog(title=request.title, body=request.body)
+    new_blog = models.Blog(title=request.title, body=request.body, user_id=1)
     db.add(new_blog)
     db.commit()
     db.refresh(new_blog)
     return new_blog
 
-@app.delete('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND)
+@app.delete('/blog/{id}', status_code=status.HTTP_404_NOT_FOUND, tags=['blogs'])
 def destroy(id, db: Session = Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id == id)
     if not blog.first():
@@ -32,7 +34,7 @@ def destroy(id, db: Session = Depends(get_db)):
     db.commit()
     return {'deleted'}
 
-@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED)
+@app.put('/blog/{id}', status_code=status.HTTP_202_ACCEPTED, tags=['blogs'])
 def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id == id)
     if not blog:
@@ -43,16 +45,33 @@ def update(id, request: schemas.Blog, db: Session = Depends(get_db)):
     return 'updated'
 
 
-@app.get('/blog')
+@app.get('/blog', response_model=List[schemas.ShowBlog], tags=['blogs'])
 def all(db: Session = Depends(get_db)):
     blogs=db.query(models.Blog).all()
     return blogs
     
-@app.get('/blog/{id}')
-def show(id, db: Session = Depends(get_db)):
+@app.get('/blog/{id}', response_model = schemas.ShowBlog, tags=['blogs'])
+def show(id, response: Response, db: Session = Depends(get_db)):
     blog=db.query(models.Blog).filter(models.Blog.id==id).first()
     if not blog:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Blog with id: {id} is not available.")
         # response.status_code = status.HTTP_404_NOT_FOUND
         # return {'detail': f"Blog with id: {id} is not available."}
     return blog
+
+@app.post('/user', response_model=schemas.ShowUser, tags=['users'])
+def create_user(request: schemas.User, db: Session = Depends(get_db)):
+    new_user=models.User(name=request.name, email=request.email, password=Hash.bcrypt(request.password))
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user 
+
+@app.get('/user/{id}', response_model = schemas.ShowUser, tags=['users'])
+def show(id, db: Session = Depends(get_db)):
+    user=db.query(models.User).filter(models.Blog.id==id).first()
+    if not user:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"User with id: {id} is not available.")
+        # response.status_code = status.HTTP_404_NOT_FOUND
+        # return {'detail': f"Blog with id: {id} is not available."}
+    return user
--- blog/schemas.py
+++ blog/schemas.py
@@ -1,5 +1,29 @@
 from pydantic import BaseModel
+from typing import List
 
-class Blog(BaseModel):
+class BlogBase(BaseModel):
     title: str
     body: str
+    
+class Blog(BlogBase):
+    class Config():
+        orm_mode = True
+        
+class User(BaseModel):
+    name: str
+    email: str
+    password: str
+    
+class ShowUser(BaseModel):
+    name: str
+    email: str
+    blogs: List[BlogBase] = []
+    class Config():
+        orm_mode = True
+        
+class ShowBlog(BaseModel):
+    title: str
+    body: str
+    creator: ShowUser
+    class Config():
+        orm_mode = True
